d17a9be0053ad44e71e37eea72565a7b31130601
h0ngwon <w___on@naver.com>
2024-08-04 15:42:20 +0900

    fix: cors

--- backend/proxyServer.js
+++ backend/proxyServer.js
@@ -1,17 +1,34 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 const PORT = 3001;
 
-app.use('/api', createProxyMiddleware({
-  target: 'https://bucket-assignment-vercel.vercel.app',
-  changeOrigin: true,
-  pathRewrite: {
-    '^/api': '/api', // URL 재작성
-  },
-}));
+app.use(cors());
+
+app.get("/api", async (req, res) => {
+	const { length = 12, type = "newest", category = 25, page = 1 } = req.query;
+
+	try {
+		const response = await axios.get(
+			"https://bucket-assignment-vercel.vercel.app/api",
+			{
+				params: {
+					length,
+					type,
+					category,
+					page,
+				},
+			}
+		);
+		res.json(response.data);
+	} catch (error) {
+		console.error("Error fetching data:", error);
+		res.status(500).json({ error: "Error fetching data" });
+	}
+});
 
 app.listen(PORT, () => {
-  console.log(`Proxy server is running on http://localhost:${PORT}`);
+	console.log(`Server is running on http://localhost:${PORT}`);
 });
